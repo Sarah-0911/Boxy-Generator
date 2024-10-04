bec44cd0264fc97821a3f6956028a15072c16a5d
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-04 13:32:57 +0200

    Donner les valeurs du state à la box ok

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -6,7 +6,7 @@ export default function ModalBtn() {
   // }
 
   return (
-    <div className="bg-blue-600 text-slate-50 py-1 px-3 mx-auto mt-2 text-sm rounded hover:bg-blue-700 cursor-pointer">
+    <div className="z-0 bg-blue-600 text-slate-50 py-1 px-3 mx-auto mt-2 text-sm rounded hover:bg-blue-700 cursor-pointer">
       {/* <button
       className=""
       onClick={handleModal}
--- src/layouts/Visualization/Visualization.jsx
+++ src/layouts/Visualization/Visualization.jsx
@@ -1,10 +1,25 @@
 import ModalBtn from "./Modal/ModalBtn";
+import getBoxShadowValue from "../../utils/getBoxShadowValue";
+import { useSelector } from "react-redux";
 
 export default function Visualization() {
+
+  const shadowsValues = useSelector(state => state.shadows);
+  const boxProperties = useSelector(state => state.box);
+
   return (
     <div className="flex flex-col ml-10 p-5 lg:ml-28">
       <ModalBtn />
-      <div className="w-[200px] h-[200px] bg-white rounded-xl mb-20 lg:mb-40">
+      <div
+      className="w-[200px] h-[200px] bg-white rounded-xl mb-20 lg:mb-40"
+      style={{
+        boxShadow: getBoxShadowValue(shadowsValues).slice(0, -1),
+        borderRadius: `${boxProperties[0].value}px`,
+        height: `${boxProperties[1].value}px`,
+        width: `${boxProperties[2].value}px`,
+        backgroundColor: boxProperties[3].value
+      }}
+      >
       </div>
     </div>
   )
