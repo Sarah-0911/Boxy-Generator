3e98768926a7776c33b455664e56684603b075e5
Sarah-0911 <sarah.tabet69@gmail.com>
2024-10-04 15:35:21 +0200

    Activer la modale ok

--- src/layouts/Visualization/Modal/ModalBtn.jsx
+++ src/layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import ModalResult from "./ModalResult";
+
 export default function ModalBtn() {
 
-  // const handleModal = () => {
-  //   console.log('coucou');
+  const [showModal, setShowModal] = useState(false);
 
-  // }
+  const closeModal = () => setShowModal(false);
 
   return (
-    <div className="z-0 bg-blue-600 text-slate-50 py-1 px-3 mx-auto mt-2 text-sm rounded hover:bg-blue-700 cursor-pointer">
-      {/* <button
-      className=""
-      onClick={handleModal}
-      >
-      </button> */}
+    <>
+      <button
+      className="z-0 bg-blue-600 text-slate-50 py-1 px-3 mx-auto mt-2 text-sm rounded hover:bg-blue-700"
+      onClick={() => setShowModal(true)}>
         Get the code!
-    </div>
+      </button>
+      {showModal && createPortal(<ModalResult closeModal={closeModal} />, document.body)}
+    </>
   )
 }
